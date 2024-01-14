1e97a00fccfc842fd67b32c1474ab97ea4f403e6
gerald <darkobekoegerald2@gmail.com>
2024-01-14 20:21:06 +0000

    completed retrofit backend

--- search/urls.py
+++ search/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('getpost', views.GetPost.as_view()),
+    path('getposts', views.GetPosts.as_view()),
     path('upload', views.Upload.as_view()),
+    path('poststuff', views.PostStuff.as_view()),
+    path('getpost/<slug:slug>', views.GetPost.as_view()),
     path('remove/<slug:slug>', views.RemovePicture.as_view()),
 ]
--- search/views.py
+++ search/views.py
@@ -8,6 +8,53 @@ from pathlib import Path
 
 
 # Create your views here.
+class GetPost(APIView):
+    def get(self, request, *args, **kwargs):
+        if self.kwargs.get("slug") is not None:
+            return Response({
+                'status': 'complete',
+                "name": "Kofi",
+                "age": int(self.kwargs.get("slug")),
+                "slug": "present"
+            })
+        else:
+            if request.GET.get("name") is not None:
+                # print("here")
+                return Response([{
+                    'status': 'complete',
+                    "name": request.GET.get("name"),
+                    "age": 400,
+                    "slug": "not present",
+                    "query": "present"
+                }])
+            else:
+                return Response({
+                    'status': 'complete',
+                    "name": "Kofi",
+                    "age": 700,
+                    "slug": "not present",
+                    "query": "absent"
+                })
+            
+
+class GetPosts(APIView):
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'status': 'complete',
+            "name": "Kofi",
+            "age": 200,
+            "query": request.GET.get("name")
+        })
+    
+
+class PostStuff(APIView):
+    def post(self, request, *args, **kwargs):
+        return Response({
+            'status': request.data.get("name"),
+        })
+
+
+
 class Upload(APIView):
     def post(self, request, *args, **kwargs):
         for image in request.FILES.values():
